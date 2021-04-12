6a196dca41b2a314d8131fd1613e212ebb9fdbdb
simonet85 <christian.85@live.fr>
2021-04-12 11:39:57 +0200

    added scrolling effects to links

--- resource/js/jquery-script.js
+++ resource/js/jquery-script.js
@@ -1,4 +1,5 @@
 $(() => {
+  // sticky nav
   $('.js--section-features').waypoint(
     (direction) => {
       if (direction === 'down') {
@@ -11,8 +12,79 @@ $(() => {
       offset: '60px',
     }
   );
+
+  // scroll on buttons
+  $('.js--scroll-to-plan').on('click', (e) => {
+    e.preventDefault();
+    let body = $('html, body');
+    body
+      .stop()
+      .animate(
+        { scrollTop: $('.js--section-plans').offset().top },
+        1000,
+        'swing'
+      );
+  });
+  $('.js--scroll-to-start').on('click', (e) => {
+    e.preventDefault();
+    let body = $('html, body');
+    body
+      .stop()
+      .animate(
+        { scrollTop: $('.js--section-features').offset().top },
+        1000,
+        'swing'
+      );
+  });
+
+  //navigation scrolling
+
+  // Select all links with hashes
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') ==
+          this.pathname.replace(/^\//, '') &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate(
+            {
+              scrollTop: target.offset().top,
+            },
+            1000,
+            function () {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(':focus')) {
+                // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              }
+            }
+          );
+        }
+      }
+    });
 });
 
 /**
  * http://imakewebthings.com/waypoints/guides/jquery-zepto/
+ * https://css-tricks.com/snippets/jquery/smooth-scrolling/
  */
