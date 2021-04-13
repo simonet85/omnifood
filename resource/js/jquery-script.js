ac423342ecab553196f98539a804f85a34c12f65
simonet85 <christian.85@live.fr>
2021-04-13 07:23:17 +0200

    Responsive nav completed

--- resource/js/jquery-script.js
+++ resource/js/jquery-script.js
@@ -82,6 +82,53 @@ $(() => {
         }
       }
     });
+
+  // Animation on scroll
+  $('.js--wp-1').waypoint(
+    (direction) => {
+      $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+    },
+    {
+      offset: '50%',
+    }
+  );
+  $('.js--wp-2').waypoint(
+    (direction) => {
+      $('.js--wp-2').addClass('animate__animated animate__fadeInUp');
+    },
+    {
+      offset: '50%',
+    }
+  );
+  $('.js--wp-3').waypoint(
+    (direction) => {
+      $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+    },
+    {
+      offset: '50%',
+    }
+  );
+  $('.js--wp-4').waypoint(
+    (direction) => {
+      $('.js--wp-4').addClass('animate__animated animate__pulse');
+    },
+    {
+      offset: '50%',
+    }
+  );
+
+  //   Mobile Nav toggle
+  $('.js--nav-icon').on('click', (e) => {
+    e.preventDefault();
+    const nav = $('.js--main-nav');
+    const icon = $('.js--nav-icon ion-icon');
+    nav.slideToggle(200);
+    if (icon.attr('name') === 'menu') {
+      icon.attr('name', 'close');
+    } else {
+      icon.attr('name', 'menu');
+    }
+  });
 });
 
 /**
